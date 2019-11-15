995d276613a0ff73ceaa119b98dbf36db82c6d4e
unknown <malei95324@163.com>
2019-11-15 21:47:14 +0800

    -m-m“create by malei:ready to pull

--- t_mall/src/main/java/com/cskaoyan/mall/controller/AdminController.java
+++ t_mall/src/main/java/com/cskaoyan/mall/controller/AdminController.java
@@ -16,8 +16,8 @@ public class AdminController {
 
     //登录
     @RequestMapping("login")
-    public String login(@RequestBody Admin admin){
+    public BaseReqVo login(@RequestBody Admin admin){
         BaseReqVo baseReqVo = adminService.login(admin);
-        return baseReqVo.toString();
+        return baseReqVo;
     }
 }
