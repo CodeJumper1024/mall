9e453dd86634e7d292787eceaf3221b85d22eb23
gaowei <gavinkoh7@gmail.com>
2019-11-15 21:54:32 +0800

    commited by gaowei

---
+++ t_mall/src/main/java/com/cskaoyan/mall/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.cskaoyan.mall.controller;
+
+import com.cskaoyan.mall.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserService userService;
+}
