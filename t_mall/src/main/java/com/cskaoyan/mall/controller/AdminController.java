5c78a40a5cdf6453aecff3e706a938c5646cd238
Wyh-JavaConfig <q583561562@163.com>
2019-11-15 21:40:20 +0800

    add login by wyh

--- t_mall/src/main/java/com/cskaoyan/mall/controller/AdminController.java
+++ t_mall/src/main/java/com/cskaoyan/mall/controller/AdminController.java
@@ -1,7 +1,9 @@
 package com.cskaoyan.mall.controller;
+import java.util.ArrayList;
 
 import com.cskaoyan.mall.bean.Admin;
 import com.cskaoyan.mall.bean.BaseReqVo;
+import com.cskaoyan.mall.bean.InfoData;
 import com.cskaoyan.mall.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +18,26 @@ public class AdminController {
 
     //登录
     @RequestMapping("login")
-    public String login(@RequestBody Admin admin){
+    public BaseReqVo login(@RequestBody Admin admin){
         BaseReqVo baseReqVo = adminService.login(admin);
-        return baseReqVo.toString();
+        return baseReqVo;
+    }
+    //显示
+    @RequestMapping("info")
+    public BaseReqVo info(String token){
+        BaseReqVo<Object> baseReqVo = new BaseReqVo<>();
+        InfoData data = new InfoData();
+        data.setAvatar("https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        data.setName("dongdong");
+        ArrayList<String> perms = new ArrayList<>();
+        perms.add("*");
+        data.setPerms(perms);
+        ArrayList<String> roles = new ArrayList<>();
+        roles.add("超级管理员");
+        data.setRoles(roles);
+        baseReqVo.setData(data);
+        baseReqVo.setErrmsg("成功");
+        baseReqVo.setErrno(0);
+        return baseReqVo;
     }
 }
