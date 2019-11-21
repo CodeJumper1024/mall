ce52822789e6d343579f88ae7a401db101f6cf33
leoku0239 <316376823@qq.com>
2019-11-21 23:59:04 +0800

    by june day20191121 23:58

--- t_mall/src/main/java/com/cskaoyan/mall/service/WxOrderService.java
+++ t_mall/src/main/java/com/cskaoyan/mall/service/WxOrderService.java
@@ -1,5 +1,6 @@
 package com.cskaoyan.mall.service;
 
+import com.cskaoyan.mall.bean.Order;
 import com.cskaoyan.mall.bean.OrderSubmitCondition;
 
 import java.util.Map;
@@ -11,4 +12,14 @@ public interface WxOrderService {
     Map<String, Object> detail(Integer orderId);
 
     Integer submit (OrderSubmitCondition orderSubmitCondition);
+
+    int prepay(String orderId);
+
+    int refund(String orderId);
+
+    int cancel(String orderId);
+
+    int confirm(String orderId);
+
+    int delete(String orderId);
 }
