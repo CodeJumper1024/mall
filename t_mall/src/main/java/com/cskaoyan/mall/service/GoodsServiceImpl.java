43bbe1c98d2a200c17b16a25711bbe7c61201db2
unknown <malei95324@163.com>
2019-11-22 11:25:52 +0800

    ml

--- t_mall/src/main/java/com/cskaoyan/mall/service/GoodsServiceImpl.java
+++ t_mall/src/main/java/com/cskaoyan/mall/service/GoodsServiceImpl.java
@@ -175,6 +175,9 @@ public class GoodsServiceImpl implements GoodsService {
         goodsMap.put("id", 1);
         int result = goodsMapper.insertByGoodsMap(goodsMap, addTime);
         int goodsId = (Integer) goodsMap.get("id");
+        String gallery = "";
+        String shareUrl = "";
+        goodsMapper.updateInfoOfLastInsert(goodsId, gallery, shareUrl);
         List<Map> specifications = (List)(info.get("specifications"));
         for (Map specification : specifications) {
             goodsSpecificationMapper.insertBySpecificationMap(specification, addTime, goodsId);
@@ -196,6 +199,8 @@ public class GoodsServiceImpl implements GoodsService {
     public Goods queryGoodsByGoodsId(Integer goodsId) {
         return goodsMapper.selectByGoodsId(goodsId);
     }
+
+    @Override
     public Goods queryGoods(Integer id) {
         return goodsMapper.selectByPrimaryKey(id);
     }
