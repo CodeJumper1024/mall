330e1c15bcae3f3c0adea261db43c0750c895e6b
youlongH <hyl20121215@163.com>
2019-11-22 15:54:49 +0800

    m

---
+++ t_mall/src/main/java/com/cskaoyan/mall/mapper/GrouponMapper.java
@@ -0,0 +1,51 @@
+package com.cskaoyan.mall.mapper;
+
+import com.cskaoyan.mall.bean.Groupon;
+import com.cskaoyan.mall.bean.GrouponExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface GrouponMapper {
+    long countByExample(GrouponExample example);
+
+    int deleteByExample(GrouponExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Groupon record);
+
+    int insertSelective(Groupon record);
+
+    List<Groupon> selectByExample(GrouponExample example);
+
+    Groupon selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Groupon record, @Param("example") GrouponExample example);
+
+    int updateByExample(@Param("record") Groupon record, @Param("example") GrouponExample example);
+
+    int updateByPrimaryKeySelective(Groupon record);
+
+    int updateByPrimaryKey(Groupon record);
+
+    int selectGrouponMemberByRuleId(@Param("ruleId") Integer RuleId);
+
+    Integer selectCountByGrouponId(@Param("grouponId") Integer grouponId);
+
+    int selectGrouponOrNotByOrderId(@Param("orderId") Integer orderId);
+
+    List<Groupon> queryGroupon();
+
+    int selectGrouponCountByNUId(Integer userId);
+
+    int selectGrouponCountByUId(Integer userId);
+
+    void insertGroupon(Groupon groupon);
+
+    int selectLastId();
+
+    void updateGrouponId(int lastId);
+
+    void setPayedTrue(int id);
+}
